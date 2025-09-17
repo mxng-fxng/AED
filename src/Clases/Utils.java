5ac3ee508d5c3006a947a0d5617114e785ca3f64
mxng-fxng <mingfang1304@gmail.com>
2025-09-17 16:30:05 +0200

    ejemplo

--- src/Clases/Utils.java
+++ src/Clases/Utils.java
@@ -19,10 +19,22 @@ Public class Utils {
 		return encontrado;
 	}
 
+	//si i1 == null entonces i2 == null o i1.equals(i2)
+	static boolean eqNull (Integer i1, Integer i2){
+		if (i1 == null) return i2 == null;
+		else return i1.equals(i2);
+	}
+
 	public static void main (String[]args){
 		ArrayIndexedList<Integer> l = new ArrayIndexedList<>(new Integer[] {1,2,3,4,5,2,-1});
 		System.out.println(l + "¿Tiene 3?" + member (l,3));
 		System.out.println(l +"¿Tiene -1?" + member (l,-1));
 		System.out.println(l +"¿Tiene 22?" + member (l,22));
+	
+		ArrayIndexedList<Integer> l2 = new ArrayIndexedList<>(new Integer[] {1,2,3,4,5,null,2,-1});
+		System.out.println(l + "¿Tiene 3?" + member (l,3));
+		System.out.println(l +"¿Tiene -1?" + member (l,-1));
+		System.out.println(l +"¿Tiene 22?" + member (l,22));
+		System.out.println(l +"¿Tiene null?" + member (l,null));
 	}
 }

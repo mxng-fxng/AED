8e57aac653ae456d5bbb63df6ac85010cb28fc38
mxng-fxng <mingfang1304@gmail.com>
2025-09-17 15:29:58 +0200

    ejemplo

---
+++ src/Clases/17sept.java
@@ -0,0 +1,21 @@
+package Clases;
+
+public class Pair <X,Y> {
+
+	private X x;
+	private Y y;
+	
+	Pair (X x, Y y){
+	this.x = x;
+	this.y = y;
+	}
+
+}
+
+Public class 17sept {
+    Public static void main (String [] args){
+        p1 = new Pair <Integer, Integer>(2,3);
+        p2 = new Pair <> (2,3);
+        p1.getX();
+    }
+}
